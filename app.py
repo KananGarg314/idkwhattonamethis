0357a7c82a0a305adb2994753fd5ef729e855a5c
KananGarg314 <kanan.g.garg2@gmail.com>
2025-08-07 13:29:18 -0400

    commit

--- app.py
+++ app.py
@@ -46,8 +46,5 @@ def index():
 
    return render_template("index.html", now=now, quote=quote, tip=tip, tasks=tasks)
 
-   app.run(debug=True)
-
-
-
 if __name__ == "__main__":
+    app.run(debug=True)
